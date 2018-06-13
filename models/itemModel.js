831f4aee21f98ced802feaa5b35acf6c4927cfa2
Ashley Moore <mooreashleyplease@gmail.com>
2018-06-13 18:57:03 -0400

    added item model

---
+++ models/itemModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+// schema constructor
+const Schema = mongoose.Schema;
+
+// item schema
+const ItemSchema = new Schema({
+
+    name: {
+        type: String,
+        trim: true
+    },
+
+    img: {
+        type: String,
+        trim: true
+    },
+
+    price: {
+        type: Number,
+        trim: true
+    }
+});
+
+const Item = mongoose.model("Item", ItemSchema);
+
+module.exports = Item;
