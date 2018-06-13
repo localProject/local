a4cf05918bc6f7a6654f0c5ad299db8e6a1aba1a
Ashley Moore <mooreashleyplease@gmail.com>
2018-06-13 19:23:29 -0400

    store page skeleton with placeholder components

---
+++ client/src/pages/LocalStore.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class LocalStore extent Component {
+
+    render() {
+        return (
+            <div>
+                // Nav
+                // Jumbotron
+                // Search
+                // ItemCardContainer
+                // ItemCard
+            </div>
+        )
+    }
+
+}
+
+export default LocalStore;
