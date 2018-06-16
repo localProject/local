d0541a7930acc6f3e550c9cec9b1649c9cb31a60
Ashley Moore <mooreashleyplease@gmail.com>
2018-06-16 11:43:09 -0400

    updated localstore page to make it similar to homepage

--- client/src/pages/LocalStore.js
+++ client/src/pages/LocalStore.js
@@ -1,15 +1,21 @@
-import React, {Component} from "react";
+import axios from 'axios';
+import React, {Component} from 'react';
+import { List, ListItem } from 'material-ui/List';
 
-class LocalStore extent Component {
+class LocalStore extends Component {
+    state = {
+        stuff: null
+    }
 
     render() {
         return (
             <div>
-                // Nav
-                // Jumbotron
-                // Search
-                // ItemCardContainer
-                // ItemCard
+                This will be the homepage.
+                <br />The navbar will go up there.
+                <br />The jumbotron will go here.
+                <br /> Then the search.
+                <br /> Then things will dynamically appear.
+                <br /> Then the footer at the bottom.
             </div>
         )
     }
