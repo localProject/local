3ada632698a9eecb0209a3f3dcb96aaa9d4bde9f
Ashley Moore <mooreashleyplease@gmail.com>
2018-06-17 17:33:23 -0400

    commenting

--- client/src/components/CompanyBox/CompanyBox.js
+++ client/src/components/CompanyBox/CompanyBox.js
@@ -3,24 +3,24 @@ import React, {Component} from "react";
 const CompanyBox = props => (
     <div className="container">
         <div className="row">
-            // name
+            /*name*/
             <div className="col-sm">{props.name}</div>
-            // address
+            /*address*/
             <div className="col-sm">
             {props.address}<br />
                 {props.city}
             </div>
-            // contact
+            /*contact*/
             <div className="col-sm">
                 {props.phone} =<br />
                 {props.email}
             </div>
-            // more information
+            /*more information*/
             <div className="col-sm">
                 <a href={props.site} target="_blank">Website</a><br />
                 <button onClick={() => props.triggerModal(props.id)}>About</button>
             </div>
-            // modal with about = props.about
+            /*modal to triggle about = props.modal*/
         </div>
     </div>
 );
