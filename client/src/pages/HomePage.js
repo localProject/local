216ecc724f76efd802bc9b605f9b567c085461e3
kayla mcfadden <kmcfadden0741@gmail.com>
2018-06-20 18:54:22 -0400

    map works, mom

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { GoogleApiWrapper } from "google-maps-react";
 import CompanyBoxContainer from "../components/CompanyBoxContainer/CompanyBoxContainer";
 import { List, ListItem } from 'material-ui/List';
 import { withUser } from '../services/withUser';
+import MapContainer from "../components/MapContainer"
 
 class HomePage extends Component {
   state = {
@@ -13,15 +15,16 @@ class HomePage extends Component {
 
     return (
 
-        <div>
-          This will be the homepage.
+      <div>
+        This will be the homepage.
           <br />The navbar will go up there.
           <br />The jumbotron will go here.
           <br /> Then the map.
-          <br /> Then the search.
+          <MapContainer google={this.props.google} />
+        <br /> Then the search.
           <hr />
-          <CompanyBoxContainer />
-          <br /> Then things will dynamically appear.
+        <CompanyBoxContainer />
+        <br /> Then things will dynamically appear.
           <br /> Then the footer at the bottom.
         </div>
 
@@ -31,4 +34,6 @@ class HomePage extends Component {
 
 // withUser function will wrap the specified component in another component that will
 // inject the currently logged in user as a prop called "user"
-export default withUser(HomePage);
+export default withUser(GoogleApiWrapper({
+  apiKey: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo"
+})(HomePage));
